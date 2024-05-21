c8349d75f43b897a310f54e0c973fdfc1a7884e1
Simão Silva <email@simaosilva.com>
2024-05-21 11:46:45 +0100

    Added Entrega 2

---
+++ Entrega-2/web/app.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+# Copyright (c) BDist Development Team
+# Distributed under the terms of the Modified BSD License.
+import os
+from logging.config import dictConfig
+
+from flask import Flask, jsonify, request
+from psycopg.rows import namedtuple_row
+from psycopg_pool import ConnectionPool
+
+# Use the DATABASE_URL environment variable if it exists, otherwise use the default.
+# Use the format postgres://username:password@hostname/database_name to connect to the database.
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://saude:saude@postgres/saude")
+
+pool = ConnectionPool(
+    conninfo=DATABASE_URL,
+    kwargs={
+        "autocommit": True,  # If True don’t start transactions automatically.
+        "row_factory": namedtuple_row,
+    },
+    min_size=4,
+    max_size=10,
+    open=True,
+    # check=ConnectionPool.check_connection,
+    name="postgres_pool",
+    timeout=5,
+)
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s:%(lineno)s - %(funcName)20s(): %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
+)
+
+app = Flask(__name__)
+app.config.from_prefixed_env()
+log = app.logger
+
+
+def is_decimal(s):
+    """Returns True if string is a parseable float number."""
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+@app.route("/", methods=("GET",))
+def clinica_index():
+
+    """Show all the clinicas"""
+
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            clinicas = cur.execute(
+                """
+                SELECT nome, morada
+                FROM clinica
+                """,
+                {},
+            ).fetchall()
+            log.debug(f"Found {cur.rowcount} rows.")
+
+    return jsonify(clinicas), 200
+
+
+#Teste
+
+
+
+
+@app.route("/ping", methods=("GET",))
+def ping():
+    log.debug("ping!")
+    return jsonify({"message": "pong!", "status": "success"})
+
+
+if __name__ == "__main__":
+    app.run()
